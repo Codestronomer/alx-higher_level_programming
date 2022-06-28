7109ba652f0717e900a49d447d45739d5163f8f9
John Rumide <johnrumide6@gmail.com>
2022-06-27 21:44:32 -0700

    [fix] 10-check_cycle function traversal

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,10 +13,21 @@ int check_cycle(listint_t *list)
 	listint_t *fast = list;
 	listint_t *slow = list;
 
-	while ((fast != NULL) || (slow->next != NULL))
+	if (list == NULL)
+		return (0);
+
+	while (fast && slow && fast->next)
 	{
-		if  (fast->next->next == slow->next)
-			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+		{
+			slow = list;
+			slow = slow->next;
+			fast = fast->next;
+			if (slow == fast)
+				return (1);
+		}
 	}
 	return (0);
 }
