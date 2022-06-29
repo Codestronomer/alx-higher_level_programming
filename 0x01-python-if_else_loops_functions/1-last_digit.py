87e38de95c2be6e5ac3c1304991ceea7071bea2c
John Rumide <johnrumide6@gmail.com>
2022-06-28 19:48:04 -0700

    [fix] add negative value to last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,8 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 is_negative = number < 0
 if is_negative:
-    str = f"Last digit of {number} is -{last_digit} "
-else:
-    str = f"Last digit of {number} is {last_digit} "
+    last_digit *= -1
+str = f"Last digit of {number} is {last_digit} "
 if last_digit > 5:
     str += "and is greater than 5"
 elif last_digit == 0:
