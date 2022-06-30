3db15bb2236227bd96a6d3afeeab40f5f616b3a7
John Rumide <johnrumide6@gmail.com>
2022-06-29 19:30:48 -0700

    add 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-i = 122
-while i > 96:
-    num = i
-    if i % 2 != 0:
-        num -= 32
-    print("{:c}".format(num), end="")
-    i -= 1
+for i in reversed(range(97, 123)):
+    print("{:c}".format(i if (i % 2 == 0) else (i - 32)), end="")
