2d6a55283130e7ad042fd0e5108003fe7805decc
John Rumide <johnrumide6@gmail.com>
2022-06-30 19:12:37 -0700

    add 3-infinite_add.py :

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("0")
+    else:
+        sum = 0
+        for arg in range(1, i+1):
+            sum += int(sys.argv[arg])
+        print(sum)
+
