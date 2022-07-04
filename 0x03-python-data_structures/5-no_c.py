dd1a9dfa0d47506d6b1d823a898c3fc613721745
John Rumide <johnrumide6@gmail.com>
2022-07-04 16:26:27 -0700

    add 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# removes all characters c and C from a string
+def no_c(my_string):
+    if not my_string:
+        pass
+    else:
+        i = len(my_string)
+        j = 0
+        n_str = ""
+        while j < i:
+            if my_string[j].lower() == 'c':
+                n_str += ''
+            else:
+                n_str += my_string[j]
+            j += 1
