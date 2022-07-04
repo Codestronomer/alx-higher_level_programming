59314261cee2a6bfdbac1e8f34df23ef46a9fd12
John Rumide <johnrumide6@gmail.com>
2022-07-04 16:10:59 -0700

    [fix] check for valid list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # prints all integers of a list, in reverse order
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list) - 1
-    while i >= 0:
-        print("{:d}".format(my_list[i]))
-        i -= 1
+    if not my_list:
+        pass
+    else:
+        i = len(my_list) - 1
+        while i >= 0:
+            print("{:d}".format(my_list[i]))
+            i -= 1
