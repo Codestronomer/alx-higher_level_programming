20d5509ae6540f179afa697fe84bdce75ddc199d
John Rumide <johnrumide6@gmail.com>
2022-07-06 20:41:01 -0700

    [fix] correct list comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    n_list = [replace for v in my_list if v == search else v]
+    n_list = [replace if v == search else v for v in my_list]
     return n_list
