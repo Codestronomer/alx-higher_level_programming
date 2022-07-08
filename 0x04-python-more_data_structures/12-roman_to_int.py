9ef3e66b0d2ea8c9e396f00dd4e2c9d333d3f98b
John Rumide <johnrumide6@gmail.com>
2022-07-07 17:32:13 -0700

    fix 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,16 @@ def roman_to_int(roman_string):
         return (0)
     rm_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
-    for i in range(len(roman_string)):
-        if i > 0 and rm_dict[roman_string[i]] > rm_dict[roman_string[i-1]]:
-            result += (rm_dict[roman_string[i]] - rm_dict[roman_string[i-1]])
+    i = 0
+    while i < len(roman_string):
+        if (i + 1 < len(roman_string)):
+            if (rm_dict[roman_string[i]] >= rm_dict[roman_string[i + 1]]):
+                result += rm_dict[roman_string[i]]
+                i += 1
+            else:
+                result += rm_dict[roman_string[i + 1]]-rm_dict[roman_string[i]]
+                i += 2
         else:
             result += rm_dict[roman_string[i]]
+            i += 1
     return (result)
