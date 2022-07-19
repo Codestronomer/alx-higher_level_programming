ca43ac2acf1166bc40b27a588eb88f42df6df0fb
John Rumide <johnrumide6@gmail.com>
2022-07-18 20:54:12 -0700

    [fix] remove indexError exceptiong

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,8 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
         except ValueError:
             pass
-        except IndexError:
-            break
         else:
             count += 1
     print()
