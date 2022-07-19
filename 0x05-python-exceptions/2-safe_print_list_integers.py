d6ec342751172e19d4b9a5d1c0b6e4a4d6daae25
John Rumide <johnrumide6@gmail.com>
2022-07-18 20:53:01 -0700

    [fix] add break statement to IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except ValueError:
             pass
         except IndexError:
-            pass
+            break
         else:
             count += 1
     print()
