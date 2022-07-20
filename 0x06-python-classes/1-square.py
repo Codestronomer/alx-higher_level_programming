45eb8243b6ec0b57abc916e2d518528c8de5bc1c
John Rumide <johnrumide6@gmail.com>
2022-07-19 18:53:10 -0700

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-square.py
+# Rumide John <johnrumide6@gmail.com>
+"""Define a new class: """
+
+
+class Square:
+    """Class Square representing a square."""
+    def __init(self, size):
+        """ Initialize the square object"""
+        self.__size = size   
