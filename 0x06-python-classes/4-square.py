78097bf41e4c3939c4db6a677c5026f62c7f74ea
John Rumide <johnrumide6@gmail.com>
2022-07-19 19:38:18 -0700

    add 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# 4-square.py
+# Rumide John <johnrumide6@gmail.com>
+"""Defines a class Square: Access and update private attrs"""
+
+
+class Square:
+    """Class Square representing a square."""
+    def __init__(self, size=0):
+        """ Initialize method that stores the size of square
+
+        Args:
+            param1 (int): size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """ Returns the square object current area"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Getter method for size attribute"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        """ Setter method for size attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
