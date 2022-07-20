b327f75014c51ffa75edf237b840f8ee32567f18
John Rumide <johnrumide6@gmail.com>
2022-07-19 19:30:04 -0700

    add 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 3-square.py
+# Rumide John <johnrumide6@gmail.com>
+"""Defines a class Square: Size validation"""
+
+
+class Square:
+    """Class Square representing a square."""
+    def __init__(self, size=0):
+        """ Initialize method that stores the size of square
+
+        Args:
+            param1 (int): size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """ Returns the square object current area"""
+        return (self.__size ** 2)
