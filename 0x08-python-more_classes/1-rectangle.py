5c380006bb98f7ae5b3799f2ac5b5829aa0b3d80
John Rumide <johnrumide6@gmail.com>
2022-07-25 16:51:11 -0700

    [fix] change init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,19 +13,8 @@ class Rectangle:
             param1 (int): width of the rectangle
             param2 (int): height of the rectangle
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
