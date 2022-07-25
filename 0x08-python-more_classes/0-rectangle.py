ae20e63d971c6bb437cdb73c26c225f62fefa23f
John Rumide <johnrumide6@gmail.com>
2022-07-25 15:47:50 -0700

    add 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# John Rumide <johnrumide6@gmail.com>
+""" Defines a rectangle class"""
+
+
+class Rectangle:
+    """ A class representing a rectangle"""
+    pass
