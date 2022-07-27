9af00f7849a126594415a9e759b0dccd8cc30b0e
John Rumide <johnrumide6@gmail.com>
2022-07-26 17:34:44 -0700

    add task files

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a LockedClass"""
+
+class LockedClass:
+    """Represents a locked class"""
+    __slots__ = ("first_name", )
+
+    def __init__(self)
+        """Initializes an instance of the class"""
+        pass
