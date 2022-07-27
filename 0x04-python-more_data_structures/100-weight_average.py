7c4ff42af59ac944881a56d2f2b47ad24e89276d
John Rumide <johnrumide6@gmail.com>
2022-07-26 19:14:24 -0700

    add 0x07-python-test_driven_development

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,9 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    average = 0
-    div = 0
-    for tup in my_list:
-        average += tup[0] * tup[1]
-        div += tup[1]
-    return float(average / div)
+    avg = 0
+    divisor = 0
+    for i in my_list:
+        avg += (i[0] * i[1])
+        divisor += i[1]
+    return float(avg/divisor)
