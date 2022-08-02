3290a37538d672cbf76394c1ba5fa50b31a84ee9
Codestronomer <johnrumide6@gmail.com>
2022-08-02 02:47:01 +0100

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 # 2-is_same_class.py
+""" File name : 2-is_same_class.py
+    It is not allowed to import any module
+"""
+
+
 def is_name_class(obj, a_class):
     return isinstance(obj, a_class)
