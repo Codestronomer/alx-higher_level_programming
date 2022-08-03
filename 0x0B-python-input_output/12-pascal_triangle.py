bfbbd8f9f1993fb3e6394ad4372a26f0fad5b9bd
John Rumide <johnrumide6@gmail.com>
2022-08-02 21:23:57 -0700

    [fix] pascal triangle logic

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,14 +8,13 @@ def pascal_triangle(n):
     """
     returns a list of lists of integers representing the pascal's triangle
     """
-    p_triangle = []
     if n <= 0:
-        return p_triangle
+        return ([])
     p_triange = [[1]]
     for i in range(1, n):
         sub_l = [1]
         prev_l = p_triangle[i - 1]
-        for j in range(1, prev_l):
+        for j in range(1, i):
             sub_l.append(prev_l[j] + prev_l[j - 1])
         sub_l.append(1)
         triangle.append(row)
