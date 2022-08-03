d3032479479cb46eee51771cd1970368bf453a8c
John Rumide <johnrumide6@gmail.com>
2022-08-02 18:29:48 -0700

    add 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Filename: 3-to_json_string.py
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    return (json.dumps(my_obj))
