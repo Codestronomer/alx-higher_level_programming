ff4525046642c0f0c71e73f96a106926b0e66866
John Rumide <johnrumide6@gmail.com>
2022-08-02 19:59:26 -0700

    add 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    filename: 10-student.py
+
+    defines a student class
+"""
+
+
+class Student:
+    """represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """
+            Initializes the student instance
+
+            Args:
+                arg1: (string)first_name
+                arg2: (string)last_name
+                arg3: (int)age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs and isinstance(attrs, list):
+            dct = {}
+            for i in attrs:
+                dct[i] = self.__dict__[i]
+            return dct
+        else:
+            return self.__dict__
