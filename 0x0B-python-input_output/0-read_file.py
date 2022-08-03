afa2c168ecdf6fb20543e87b1d15db97a088f5ba
John Rumide <johnrumide6@gmail.com>
2022-08-02 18:17:10 -0700

    add tasks files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Filename: 0-read_file.py
+"""
+
+
+def read_file(filename=""):
+    """reads a text file and prints it to stdout"""
+    if filename:
+        with open(filename, encoding="utf-8") as f:
+            print(f.read())
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    filename: 1-write_file.py
+"""
+
+def write_file(filename="", text=""):
+    """writes a string to a text file and return character length"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return (f.write(text))
