f0004c5ca9982ad193c5b7532a0c2ea032dbc429
John Rumide <johnrumide6@gmail.com>
2022-08-02 18:21:14 -0700

    [fix] syntx

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
     filename: 1-write_file.py
 """
 
+
 def write_file(filename="", text=""):
     """writes a string to a text file and return character length"""
     with open(filename, "w", encoding="utf-8") as f:
