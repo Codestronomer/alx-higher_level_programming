3761d97f9a35ac89096216cce181b63defc60285
John Rumide <johnrumide6@gmail.com>
2022-10-03 12:21:10 -0700

    [update] add project files

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+if (argv.length === 1) {
+  console.log('No Argument');
+} else if (argv.length === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
