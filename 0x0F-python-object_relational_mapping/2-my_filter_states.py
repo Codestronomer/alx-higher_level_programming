578d849714263968d3285a572888d9e19491821f
John Rumide <johnrumide6@gmail.com>
2022-10-09 09:01:34 -0700

    [update] add 2-my_filter_states.py, fix 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 2-my_filter_states.py
+
+"""
+A script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connection
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    # Query
+    cur.execute(
+            "SELECT * FROM states WHERE name='{name}' ORDER BY states.id"
+            .format(name=argv[4])
+            )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close connection
+    cur.close()
+    db.close()
