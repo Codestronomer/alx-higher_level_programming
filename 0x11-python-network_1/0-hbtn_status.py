105b259fe817bb977f3f20f4ee3707dbd47c5b9d
John Rumide <johnrumide6@gmail.com>
2022-10-21 17:00:11 -0700

    [fix] remove unneccesary function

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,4 @@ import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        print(dir(response))
         print(response.read())
