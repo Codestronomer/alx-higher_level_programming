ebf97a9bbe83196de9c20b4d1f1b9c1857610ee8
John Rumide <johnrumide6@gmail.com>
2022-10-22 09:34:48 -0700

    [fix] add print statement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     try:
         url = sys.argv[1]
         with urllib.request.urlopen(url) as response:
-            page = response.read().decode(encoding="utf-8")
+            print(response.read().decode(encoding="utf-8"))
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
