e5a33a5849ecf88391c494664e965c341d8e4368
John Rumide <johnrumide6@gmail.com>
2022-10-21 17:51:50 -0700

    [fix] decode page

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,10 +10,11 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {'email': f'{sys.argv[2]}'}
+    value = {'email': sys.argv[2]}
 
     data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        page = response.read()      
+        page = response.read()
+        print(page.decode(encoding="utf-8"))
