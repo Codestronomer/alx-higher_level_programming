14509356dc2087ce012054f321a9713a82d5956c
codestronomer <johnrumide6@gmail.com>
2022-11-15 06:41:30 -0800

    [fix] add files

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const process = require('process');
 
 // opens gets response statusCode from url passed as argument
 request(process.argv[2], (err, response, body) => {
-  // console.error("error:", err);
-  console.log("code: ", response && response.statusCode);
+  if (err) { console.log(err); }
+  console.log('code: ', response && response.statusCode);
 });
