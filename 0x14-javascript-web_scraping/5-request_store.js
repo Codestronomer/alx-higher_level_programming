0fccc70f52919d03c712f22d144338a0214d860d
codestronomer <johnrumide6@gmail.com>
2022-11-15 21:30:55 +0100

    [fix] add condition

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,6 +4,10 @@ const request = require('request');
 
 request(process.argv[2], (err, response, body) => {
   if (err == null) {
-    fs.writeFilesync(process.argv[3], body);
+    if (response && response.statusCode == 200) {
+      fs.writeFile(process.argv[3], body, (err) => {
+        if (err) { console.log(err) }
+      });
+    }
   }
 });
