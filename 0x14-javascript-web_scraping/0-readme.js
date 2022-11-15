a14f69b4aa903538dd108394745734a5db700143
codestronomer <johnrumide6@gmail.com>
2022-11-15 14:15:25 +0100

    [add] 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+
+// opens file passed as argument
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+        if (err) {
+                console.log(err);
+                return;
+        }
+        console.log(data);
+});
