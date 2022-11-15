1130e6a5de3f8987651f4029a761a64b9f4a5f11
codestronomer <johnrumide6@gmail.com>
2022-11-15 16:45:47 +0100

    [update] add code for starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+// get starwars title from episode passed in argument
+request(process.argv[2],
+  (err, response, body) => {
+    if (err == null) {
+      const res = JSON.parse(response.body);
+      // console.log(res.results);
+      movie_count = 0;
+      res.results.forEach(movie => {
+        movie.characters.forEach(actor => {
+          if (actor.includes('18')) {
+            movie_count++;
+          };
+        });
+      });
+      console.log(movie_count);
+    };
+  });
