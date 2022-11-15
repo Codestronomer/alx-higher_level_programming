6cb430ef34c7b744c52ff4a90327ee064e80942b
codestronomer <johnrumide6@gmail.com>
2022-11-15 22:58:58 +0100

    [fix] add 100=starwars_characters code logic

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+// get starwars title from episode passed in argument
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err == null) {
+      const res = JSON.parse(response.body);
+      characters = []
+      res.characters.forEach(url => {
+        request(url, (err, response, body) => {
+          const res = JSON.parse(response.body);
+          console.log(res.name);
+        });
+      });
+    }
+  });
