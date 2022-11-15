fdd6fd3167f5d701eea4db58b2d91571b38076f5
codestronomer <johnrumide6@gmail.com>
2022-11-15 05:24:49 -0800

    [fix] syntax and permission

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const process = require('process');
 
 // opens file passed as argument
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-        if (err) {
-                console.log(err);
-                return;
-        }
-        console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
