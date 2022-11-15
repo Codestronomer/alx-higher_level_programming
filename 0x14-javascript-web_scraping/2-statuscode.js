90ee80c0062d802cc3d393370bbf68c619381713
codestronomer <johnrumide6@gmail.com>
2022-11-15 06:56:54 -0800

    [fix] correct code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
-
 // opens gets response statusCode from url passed as argument
 request(process.argv[2], (err, response, body) => {
-  if (err) { console.log(err); }
-  console.log('code: ', response && response.statusCode);
+  if (err == null) {
+    console.log('code: ' + response.statusCode);
+  }
 });
