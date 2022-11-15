f3a98e57e5fabbb12121d149a6e23a4b6e840995
codestronomer <johnrumide6@gmail.com>
2022-11-15 20:55:49 +0100

    add 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request')
 
+request(process.argv[2], (err, response, body) => {
+  if (err == null) {
+    content = body;
+    fs.writeFilesync(process.argv[3], body);
+  }
+})
