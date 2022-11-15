bba3505986a8a53d2bf298b5d6fe38f6ea7374a3
codestronomer <johnrumide6@gmail.com>
2022-11-15 12:03:33 -0800

    [fix] correct syntax

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const request = require('request')
+const request = require('request');
 
 request(process.argv[2], (err, response, body) => {
   if (err == null) {
-    content = body;
     fs.writeFilesync(process.argv[3], body);
   }
-})
+});
