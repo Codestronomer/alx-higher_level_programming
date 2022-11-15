d93ae0bdcc0aed20d82309dc64f05790f5e734f0
codestronomer <johnrumide6@gmail.com>
2022-11-15 21:58:30 +0100

    [fix] change function syntax

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], function (err, response, body) {
+request(process.argv[2], (err, response, body) => {
   if (err == null) {
     const resp = {};
     const json = JSON.parse(body);
